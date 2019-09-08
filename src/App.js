a55e4af82b3d76c19addaaac85f2b5abf432ff3b
Matthew Chick <c.chick@unsw.edu.au>
2019-09-08 19:55:34 +1000

    Change functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// class component
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/* functional component
 function App() {
   return (
     <div className="App">
@@ -22,5 +47,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
