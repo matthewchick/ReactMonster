4f7e28bbc917a9f7b5796cd3ed55a4c293615d12
Matthew Chick <c.chick@unsw.edu.au>
2019-09-10 20:48:14 +1000

    Add component lifecycle and use ES6 promise

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';   //destructing
 import './App.css';
 
 // class component
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {   // promise in ES6
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())   //convert into json format
+    .then(users => this.setState({ monsters: users }));
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+          ))}
       </div>
     );
   }
 }
 
-/* functional component
-function App() {
+/* functional or stateless component
+function App() {s
   return (
     <div className="App">
       <header className="App-header">
